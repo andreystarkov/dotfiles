4943eedf008e60113eb8e92d822728b1d4b0c1e9
Andrey Starkov <im@andreystarkov.ru>
2018-12-14 06:30:40 +0300

    fix setup, add colorls/zgen/vim.plug install

---
+++ scripts/iterm-colors-install.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/dotfiles/iterm-color-schemes
--- setup.sh
+++ setup.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+
+if [[ ! -f $HOME/.zgen/zgen.zsh ]]; then
+  git clone https://github.com/tarjoilija/zgen $HOME/.zgen
+fi
+
+if ! [ -x "$(command -v colorls)" ]; then
+  brew install colorls
+fi
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 rm -rf ~/.zshrc ~/.zsh_aliases ~/.zsh_functions ~/.zgen-setup
 
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/dotfiles/zsh/zsh_aliases ~/.zsh_aliases
 ln -s ~/dotfiles/zsh/zsh_functions ~/.zsh_functions
 ln -s ~/dotfiles/zsh/zgen-setup ~/.zgen-setup
-ln -s ~/dotfiles/vimrc.js/vimrc ~/.vimrc`
+ln -s ~/dotfiles/vimrc.js/vimrc ~/.vimrc
+ln -s ~/dotfiles/vimrc.js ~/.vimrc.js
 
 mkdir -p ~/.config
 
 ln -s ~/dotfiles/colorls ~/.config/colorls
+
+zsh
