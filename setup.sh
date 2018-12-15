db749f534eecfa012325cd0540ae758a5741a09c
Dmitriy Vorontsov <203123@gmail.com>
2018-12-15 11:08:50 +0300

    edit

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-bash ./install.sh
+source ~/dotfiles/install.sh
 
-rm -rf ~/.zshrc ~/.zsh_aliases ~/.zsh_functions ~/.zgen-setup
+rm -rf ~/.zshrc ~/.zsh_aliases ~/.vimrc ~/.zgen-setup ~/.zsh_functions
 
-ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
-ln -s ~/dotfiles/zsh/zsh_aliases ~/.zsh_aliases
-ln -s ~/dotfiles/zsh/zsh_functions ~/.zsh_functions
-ln -s ~/dotfiles/zsh/zgen-setup ~/.zgen-setup
-ln -s ~/dotfiles/vimrc.js/vimrc ~/.vimrc
+ln -s ~/dotfiles/zsh/zshrc.zsh ~/.zshrc
+ln -s ~/dotfiles/zsh/zsh_aliases.zsh ~/.zsh_aliases
+ln -s ~/dotfiles/zsh/zsh_functions.zsh ~/.zsh_functions
+ln -s ~/dotfiles/zsh/zgen-setup.zsh ~/.zgen-setup
+ln -s ~/dotfiles/vimrc.js/vimrc §~/.vimrc
 ln -s ~/dotfiles/vimrc.js ~/.vimrc.js
 
 mkdir -p ~/.config
 mkdir -p ~/.config/nvim§
 
 ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-§
+
 ln -s ~/dotfiles/colorls ~/.config/colorls
 
 zsh
